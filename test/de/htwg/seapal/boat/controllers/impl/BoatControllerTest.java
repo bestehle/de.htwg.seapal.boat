d4b6c0e9dca1c4a6600b1ea8fda0939830844c04
bestehle <bestehle@htwg-konstanz.de>
2013-04-10 11:55:02 +0200

    add empty test

---
+++ test/de/htwg/seapal/boat/controllers/impl/BoatControllerTest.java
@@ -0,0 +1,24 @@
+package de.htwg.seapal.boat.controllers.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BoatControllerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetString() {
+		;
+	}
+
+}
